5f222043c071dde53549af10002289f6ec4ab479
Soumya Goyal <somyagoyal739@gmail.com>
2019-09-04 22:57:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,12 @@ def monopoly_with_third_tier
   {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {}
+      :rent_in_dollars => {
+        :jan => "$23",
+        :feb => "$25",
+        :mar => "$22",
+        :april => "$24"
+      }
     }
     
   }
